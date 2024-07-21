86d201aaad9e1af47bdb72be6b42f4eeadf63bee
Someguy8315 <dimadragos17@gmail.com>
2024-07-21 11:10:51 +0300

    initial shared lib setup

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include "func.h"
 
 int main()
 {
     int arr[3];
     arr[2] = 5;
+
+    int value = getValue();
+    printf("value = %d\n", value);
 
     puts("finished execution...");
     return 0;
---
+++ src/func.c
@@ -0,0 +1,6 @@
+#include "func.h"
+
+int getValue()
+{
+    return 500;
+}
